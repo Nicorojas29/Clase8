06c992aeefb659a5b8d5184c6bb55d7dbb73e7ae
Nicorojas29 <nicolassrojas.29@gmail.com>
2023-05-15 15:30:16 -0300

    comenzando con express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("Hola Mundo");
+const express = require('express')
+const PORT = 3001
+
+const app = express() /* ejecuto la funcion express */
+
+app.get("/", (req, res)=> {
+    res.status(200).json({"Mensaje": "Hola Mundo!"})
+})
+
+
+app.listen(PORT, ()=> (console.log(`A romperla en el puerto ${PORT}`))) // corro la aplicacion que corre en el puerto 3001
+

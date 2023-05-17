0b071b8fc1be75ea673298591f95812a2d362af5
Nicorojas29 <nicolassrojas.29@gmail.com>
2023-05-16 23:35:11 -0300

    Get y Post

--- src/controladores/productosController.js
+++ src/controladores/productosController.js
@@ -1,11 +1,51 @@
-const getProductos = (req, res) =>{
+const productos = require('../../data/productos.json') // importo los productos
+const tipoProducto = require('../../data/tipoProducto.json') // importo para buscar los tipo de prod
+
+const getProductos = (req, res) => {
     res.status(200).json({"Mensaje": "Hola Mundo!"})
 }
 
-const getProductosById = (req, res) => {
+const getProductoById = (req, res) => {
     const id = req.params.id  // params sirve para recuperar el id
-    res.status(200).json({"Mensaje": `Consulto el producto ${id}`})
+    const idx = productos.findIndex ( prod => prod.idProducto ==id)
+    if (idx >= 0) {  // si es mayor a 0 el producto existe
+        const idxtp = tipoProducto.findIndex(  // para saber si tiene categoria y esta habilitada
+            tp => tp.idTipoProducto == productos[idx].idTipoProducto && tp.enable)
+        if (idxtp >= 0) {
+                const producto = productos[idx]
+                const tipo = tipoProducto[idxtp]
+                delete producto.idTipoProducto
+                delete tipo.enable
+                res.status(200).json({...producto, tipo})
+         } else {
+            res.status(404).json({mensaje:`No se encontro o esta deshabiliatdo el tipo de producto asociado ${productos[idx].idTipoProducto}`})
+         }
+    } else {
+        res.status(404).json({mensaje:`No se encontro el producto ${id}`})
+    }
+}
+ 
+const postProducto = (req, res) => {
+    const bodyRecibido = req.body  //lo que recibo en el req.body es el objeto que voy a recibir
+    const idxtp = tipoProducto.findIndex(  // para saber si tiene categoria y esta habilitada
+        tp => tp.idTipoProducto==bodyRecibido.idTipoProducto && tp.enable)
+    if ( idxtp >=0 ) {
+        let maxId = 1
+        if (productos.length > 0)  {  //length es una propiedad para ver si esta vacio
+            const idsProducto = productos.map (p => p.idProducto) 
+            maxId = Math.max(...idsProducto) + 1 
+        }
+        const producto =  { 
+            idProducto: maxId,
+            descripcion: bodyRecibido.descripcion,
+            stock: bodyRecibido.stock,
+            idTipoProducto: bodyRecibido.idTipoProducto
+        }
+        productos.push(producto)  // push para agregar un elemento dentro de la coleccion de productos
+        res.status(201).json(producto)
+    } else {
+        res.status(400).json({mensaje: `Error en el codigo en el tipo de producto. No existe el codigo ${bodyRecibido.idTipoProducto}`})
+    }
 }
 
-
-module.exports = {getProductos, getProductosById}
+module.exports = {getProductos, getProductoById, postProducto}
--- src/rutas/productosRuta.js
+++ src/rutas/productosRuta.js
@@ -1,10 +1,11 @@
 const express = require ('express') // configuro la ruta con el router
-const { getProductos, getProductosById } = require('../controladores/productosController')
+const { getProductos, getProductoById, postProducto } = require('../controladores/productosController')
 
 const productosRuta = express.Router()
 
 productosRuta.get("/", getProductos)
-productosRuta.get("/:id", getProductosById)
+productosRuta.get("/:id", getProductoById)
+productosRuta.post("/", postProducto)
                  
 module.exports = {productosRuta} // atributo y valor en un mismo nombre
 
